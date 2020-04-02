2e6303cd1990f6f55e8eabe7f6c53cac7cbd63de
winnyiee@naver.com <winnyiee@naver.com>
2020-04-02 19:18:56 +0900

    naver login

---
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -0,0 +1,114 @@
+package com.example.demo.controller;
+
+
+
+import org.apache.tomcat.util.json.JSONParser;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.net.UnknownHostException;
+import java.security.SecureRandom;
+import java.text.ParseException;
+import java.util.Map;
+
+@Controller
+public class LoginController {
+
+    private String CLIENT_ID = "_L3yUfmDgCWHvk7vDar5";
+    private String CLI_SECRET = "K0m6RtM4Hh";
+
+
+    @RequestMapping("/login")
+    public String testNaver(HttpSession session, Model model) throws UnsupportedEncodingException, UnknownHostException {
+        System.out.println("aa");
+        String redirectURI = URLEncoder.encode("http://localhost:8080/naver/callback1", "UTF-8");
+        SecureRandom random = new SecureRandom();
+        String state = new BigInteger(130, random).toString();
+        String apiURL = "https://nid.naver.com/oauth2.0/authorize?response_type=code";
+        apiURL += String.format("&client_id=%s&redirect_uri=%s&state=%s",
+                CLIENT_ID, redirectURI, state);
+        session.setAttribute("state", state);
+        model.addAttribute("apiURL", apiURL);
+        return "/login";
+    }
+
+    @RequestMapping("/naver/callback1")
+    public String naverCallback1(HttpSession session, HttpServletRequest request, Model model) throws IOException, org.apache.tomcat.util.json.ParseException {
+        String code = request.getParameter("code");
+        String state = request.getParameter("state");
+        String redirectURI = URLEncoder.encode("http://localhost:8080/naver/callback1", "UTF-8");
+        String apiURL;
+        apiURL = "https://nid.naver.com/oauth2.0/token?grant_type=authorization_code&";
+        apiURL += "client_id=" + CLIENT_ID;
+        apiURL += "&client_secret=" + CLI_SECRET;
+        apiURL += "&redirect_uri=" + redirectURI;
+        apiURL += "&code=" + code;
+        apiURL += "&state=" + state;
+        System.out.println("apiURL=" + apiURL);
+        String res = requestToServer(apiURL);
+        if (res != null && !res.equals("")) {
+            model.addAttribute("res", res);
+            Map<String, Object> parsedJson = new JSONParser(res).parseObject();
+            System.out.println(parsedJson);
+            session.setAttribute("currentUser", res);
+            session.setAttribute("currentAT", parsedJson.get("access_token"));
+            session.setAttribute("currentRT", parsedJson.get("refresh_token"));
+        } else {
+            model.addAttribute("res", "Login failed!");
+        }
+        return "home";
+
+    }
+
+    private String requestToServer(String apiURL) throws IOException {
+        return requestToServer(apiURL, "");
+    }
+
+    private String requestToServer(String apiURL, String headerStr) throws IOException {
+        URL url = new URL(apiURL);
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+        System.out.println("header Str: " + headerStr);
+        if (headerStr != null && !headerStr.equals("")) {
+            con.setRequestProperty("Authorization", headerStr);
+        }
+        int responseCode = con.getResponseCode();
+        BufferedReader br;
+        System.out.println("responseCode=" + responseCode);
+        if (responseCode == 200) { // 정상 호출
+            br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        } else {  // 에러 발생
+            br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+        }
+        String inputLine;
+        StringBuffer res = new StringBuffer();
+        while ((inputLine = br.readLine()) != null) {
+            res.append(inputLine);
+        }
+        br.close();
+        if (responseCode == 200) {
+            return res.toString();
+        } else {
+            return null;
+        }
+    }
+
+    @RequestMapping("/naver/invalidate")
+    public String invalidateSession(HttpSession session) {
+        session.invalidate();
+        return "redirect:/";
+
+    }
+}
